da952cae5be21094ee9a5eff1bdbb9d173c711fb
Crowdbotics <engineering@crowdbotics.com>
2023-07-04 18:21:55 +0000

    Initial commit.

---
+++ backend/newapppppppp_dev_97423/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for newapppppppp_dev_97423 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'newapppppppp_dev_97423.settings')
+
+application = get_wsgi_application()
